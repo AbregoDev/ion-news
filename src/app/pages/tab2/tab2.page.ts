3ecda20fb374886f016a2fdfcacbdea9946385ec
Bryan Moreno <abre.go@outlook.com>
2023-12-21 16:33:13 -0600

    feat: Add news categories as segment buttons

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,16 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
+import {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonSegment,
+    IonSegmentButton,
+    IonLabel
+} from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss'],
-  standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+    selector: 'app-tab2',
+    templateUrl: 'tab2.page.html',
+    styleUrls: ['tab2.page.scss'],
+    standalone: true,
+    imports: [
+        CommonModule,
+        IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+        IonSegment,
+        IonSegmentButton,
+        IonLabel,
+    ]
 })
 export class Tab2Page {
 
-  constructor() {}
+    categories: readonly string[] = [
+        'business',
+        'entertainment',
+        'general',
+        'health',
+        'science',
+        'sports',
+        'technology',
+    ];
+    selectedCategory: string = this.categories[0];
 
+    constructor() { }
+
+    segmentChanged(event: any) {
+        
+    }
 }
