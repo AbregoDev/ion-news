07e8f069bdf7fbf4cd7d20b9475d6506ee42b6e6
Bryan Moreno <abre.go@outlook.com>
2023-12-26 20:20:55 -0600

    style: Add semicolon

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -77,6 +77,6 @@ export class Tab2Page implements OnInit {
                 
                 this.articles = articles;
                 this.infiniteScroll.complete();
-            })
+            });
     }
 }
