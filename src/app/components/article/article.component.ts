d3d40e079d3988868a3ed3f3264fbe74ebde069a
Bryan Moreno <abre.go@outlook.com>
2023-12-28 14:42:19 -0600

    feat: Open articles on external browser

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,7 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
-import { IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonImg } from '@ionic/angular/standalone';
+import { Component, Input } from '@angular/core';
+import {
+    IonButton,
+    IonCard,
+    IonCardContent,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCol,
+    IonIcon,
+    IonImg,
+    IonRow
+} from '@ionic/angular/standalone';
+
 import { Article } from 'src/app/interfaces/news.interface';
+
+import { addIcons } from 'ionicons';
+import { ellipsisVerticalOutline } from 'ionicons/icons';
+import { Browser } from '@capacitor/browser';
 
 @Component({
     selector: 'app-article',
@@ -15,15 +30,24 @@ import { Article } from 'src/app/interfaces/news.interface';
         IonCardTitle,
         IonCardContent,
         IonImg,
+        IonRow,
+        IonCol,
+        IonButton,
+        IonIcon,
     ],
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent {
 
     @Input() index: number = 0;
     @Input() article!: Article;
 
-    constructor() { }
+    constructor() {
+        addIcons({ ellipsisVerticalOutline });
+    }
 
-    ngOnInit() { }
+    openArticle() {
+        Browser.open({ url: this.article.url });
+    }
 
+    openOptions() { }
 }
