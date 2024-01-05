a02f1ba584d30f8bd001a401b8f1a9db5bbdc245
Bryan Moreno <abre.go@outlook.com>
2024-01-04 21:29:15 -0600

    feat: Show favorite articles on tab

--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
+import { StorageService } from 'src/app/services/storage.service';
+import { Article } from 'src/app/interfaces/news.interface';
+import { ArticlesComponent } from 'src/app/components/articles/articles.component';
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss'],
-  standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+    selector: 'app-tab3',
+    templateUrl: 'tab3.page.html',
+    styleUrls: ['tab3.page.scss'],
+    standalone: true,
+    imports: [
+        IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+        ArticlesComponent,
+        ExploreContainerComponent
+    ],
 })
 export class Tab3Page {
-  constructor() {}
+
+    get articles(): Article[] {
+        return this.storageService.localArticles;
+    }
+
+    constructor(private storageService: StorageService) { }
+
+
 }
